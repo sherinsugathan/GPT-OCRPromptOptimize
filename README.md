52c409a605687f950486407ff56fcceae73181ea
Sherin Sugathan <47589782+sherinsugathan@users.noreply.github.com>
2023-12-29 20:13:21 +0100

    cleanup + command line processing
    Added support for command line processing
    minor cleanups

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# GPT-OCRPromptOptimize
-A GPT prompt optimizing setup for improving OCR accuracy.
+# GPT-OCR Prompt Optimize
+The scripts in this project can be used for optimizing the prompt used for correcting OCR engine responses. All the necessary data has been provided under the directory `/data/`
+## Setup and Running
+1. Clone the repository - `git clone https://github.com/your-username/your-repository.git`
+2. Install dependencies.
+   `pip install -r requirements.txt`
+3. Obtain your OpenAPI key and set it to your local environment variable `OPENAI_API_KEY`
+4. An example run script:
+
+ `python src/main.py 'cjeu-35-turbo-instruct' 'Please note that the text to be corrected is in French. Fix spelling mistakes, do not add/remove words, make consistent word spacing, add missing spaces, fix font case issues within words, fix numbering issues, make consistent line breaks for the following text:'`
+
+## Results
+text here 
+
+## Warning
+The use of OpenAI's API is not free and is subject to the pricing policies set by OpenAI. Usage costs are incurred based on the number of tokens processed. Detailed pricing information can be found on the OpenAI pricing page. If you choose to run or integrate this code, any and all charges incurred for API usage will be billed to the client of this specific project. We urge you to be mindful of potential bugs or unnecessary loops in the code that might lead to excessive and unintended API calls. Such issues can result in unexpected charges to the client. It's recommended to thoroughly test the code in a controlled environment before extensive usage.
--- src/main.py
+++ src/main.py
@@ -8,8 +8,12 @@ import Levenshtein
 import difflib
 import re
 import time
+import argparse
 from pathlib import Path
 from datetime import datetime
+import matplotlib.pyplot as plt
+import zipfile
+from scipy.interpolate import make_interp_spline
 
 def preprocess_text(text):
     # Replace multiple spaces with a single space
@@ -19,6 +23,18 @@ def preprocess_text(text):
     # Remove spaces at the beginning and end of the text
     text = text.lower().strip()
     return text
+
+def extract_zip(zip_path):
+    """
+    Extracts a zip file to the same directory where the zip file is located.
+    :param zip_path: Path to the zip file.
+    """
+    # Determine the directory where the zip file is located
+    extract_to = os.path.dirname(zip_path)
+    print("Please wait! Extracting...")
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+    print(f"Extracted {zip_path} to {extract_to}")
 
 def normalized_levenshtein_distance(s1, s2):
     if not s1 and not s2:  # Both strings are empty
@@ -32,7 +48,6 @@ def normalized_levenshtein_distance(s1, s2):
 def proc_text4(text_in):
     # Call GPT-4 chat
     theprompt = prompt_prefix + '\n""""' + text_in + '"""'
-
     openai.api_version = "2023-07-01-preview"
     response = openai.ChatCompletion.create(
         engine="cjeu-4",
@@ -66,26 +81,16 @@ def proc_text_instruct(text_in):
     #return gpt_out, response, theprompt
     return gpt_out
 
-def fix_cjeu(text):
-    # Construct a chat-based prompt for ChatGPT
-    message = {
-        "role": "system",
-        "content": "Consider the OCR output text and make corrections. The language is French."
-    }
-    user_message = {
-        "role": "user",
-        "content": f"Please note that the text to be corrected is in French. Fix spelling mistakes, do not add/remove words, make consistent word spacing, add missing spaces, fix font case issues within words, fix numbering issues, make consistent line breaks for the following text: '{text}'"
-    }
 
-    # Use the Chat Completion endpoint of the API
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # ChatGPT model
-        messages=[message, user_message]
-    )
-
-    # Extract and return the assistant's response
-    assistant_message = response.choices[0].message['content']
-    return assistant_message
+parser = argparse.ArgumentParser(description='Run a prompt on cjeu french text.')
+# Adding arguments
+parser.add_argument('engine', type=str, choices=['cjeu-35-turbo-instruct', 'cjeu-4'], help='Name of the GPT Engine')
+parser.add_argument('prompt', type=str, help='The prompt string')
+# Parsing arguments
+args = parser.parse_args()
+#prompt_prefix = "Please note that the text to be corrected is in French. Fix spelling mistakes, do not add/remove words, make consistent word spacing, add missing spaces, fix font case issues within words, fix numbering issues, make consistent line breaks for the following text:"
+prompt_prefix = args.prompt
+engineSelection = args.engine
 
 current_dir = Path(__file__).resolve().parent
 parent_dir = current_dir.parent
@@ -94,6 +99,17 @@ gcv_file = data_root + "all_data.csv"
 aocr_file = data_root + "fr/"
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 gpt_result_folder = aocr_file + "/GPT_" + timestamp + "/"
+gpt_image_result_fileName = data_root + "GPT_" + timestamp + ".png"
+
+zip_path = data_root + "all_data.zip"
+zip_file_path = data_root + "all_data.csv"
+
+if os.path.exists(zip_file_path) == False:
+    # ground truth data extraction.
+    if os.path.exists(zip_path):
+        print(f"Zip file found: {zip_path}")
+        # Extract the zip file
+        extract_zip(zip_path)
 
 # Check if the folder exists
 if not os.path.exists(gpt_result_folder):
@@ -103,7 +119,7 @@ if not os.path.exists(gpt_result_folder):
 else:
     print(f"Folder already exists: {gpt_result_folder}")
 
-prompt_prefix = "Please note that the text to be corrected is in French. Fix spelling mistakes, do not add/remove words, make consistent word spacing, add missing spaces, fix font case issues within words, fix numbering issues, make consistent line breaks for the following text:"
+
 # Set up API
 openai.api_type = "azure"
 openai.api_base = "https://gpt-cjeuscans-test-swe.openai.azure.com/"
@@ -141,8 +157,12 @@ for fileName in file_list:
             with open(file_path, 'r', encoding='utf-8') as file:
                 text_to_correct = file.read()
 
-            # perform gpt based correction.
-            corrected_text = proc_text_instruct(text_to_correct)
+            if(engineSelection == "cjeu-35-turbo-instruct"):
+                # perform gpt based correction.
+                corrected_text = proc_text_instruct(text_to_correct)
+            if(engineSelection == "cjeu-4"):
+                # perform gpt based correction.
+                corrected_text = proc_text4(text_to_correct)
 
             # Example usage:
             distance_old = normalized_levenshtein_distance(preprocess_text(gcv_raw_text), preprocess_text(gcv_corrected_text))
@@ -161,4 +181,25 @@ for fileName in file_list:
             print("Error occurred. Retrying...")
             time.sleep(2)
 
+# Plotting
+df = pd.read_csv(distance_result_file, delim_whitespace=True, header=None)
+# Assigning columns to variables
+X = df.iloc[:, 0]   # First column
+Y1 = df.iloc[:, 2]  # Third column
+Y2 = df.iloc[:, 3]  # Fourth column
+
+# Plotting Y1 and Y2 over X
+plt.figure(figsize=(30, 6))
+plt.plot(X, Y1, label='Google Vision', color='blue', linestyle='-', marker='o')
+plt.plot(X, Y2, label='Adobe+GPT', color='green', linestyle='-', marker='o')
+plt.xlabel('X')
+plt.ylabel('Editing Distance')
+plt.title('Normalized Levenshtein Distance comparison between Google Vision and Adobe+GPT')
+plt.legend()
+even_X_labels = [x for x in X if x % 2 == 0]
+plt.xticks(even_X_labels)
+#plt.xticks(X)
+#plt.grid(True)
+plt.savefig(gpt_image_result_fileName, format='png', dpi=300, bbox_inches='tight', transparent=False, orientation ='landscape')
+plt.show()
 print("###### COMPLETED ########")
