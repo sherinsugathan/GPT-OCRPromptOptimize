09e7a59687c9350ee96fffacf6915fd64da37c08
Sherin Sugathan <47589782+sherinsugathan@users.noreply.github.com>
2024-01-02 13:08:02 +0100

    update
    update

--- README.md
+++ README.md
@@ -1,25 +1,41 @@
 # GPT-OCR Prompt Optimize
-The scripts in this project can be used for finding an optimized prompt for correcting OCR engine responses. All the raw OCR output has been provided under the directory `/data/`. Each txt file in the data folder represent a single page having a multi-column layout in French language. Running the `main.py` will give you a comparison results between normalized levenshtein distance for Google Vision and Adobe+GPT in both textual and graphical forms. Finding the prompt that gives better accuracy is done manually by trial-and-error method.
+This project's scripts are designed to optimize prompts for enhancing OCR engine outputs. The raw OCR data is stored in the `/data/` directory. Each text file within this folder corresponds to a single page with a multi-column layout, written in French. Executing `main.py` produces a comparative analysis of the normalized Levenshtein distance for both Google Vision and Adobe+GPT, presented through textual and graphical outputs. The process of crafting a more accurate prompt is currently manual, relying on a trial-and-error approach. Below is an illustrative baseline result for the `cjeu-4` engine, showcasing the prompt utilized in our sample cases.
+
+![Alt text](baselineResult.png)
+
 ## Setup and Running
 1. Clone the repository - `git clone https://github.com/your-username/your-repository.git`
 2. Install dependencies.
    `pip install -r requirements.txt`
 3. Obtain your OpenAPI key and set it to your local environment variable `OPENAI_API_KEY`
-4. An example run script:
+4. Run
 
  ```python
- python src/main.py 'cjeu-35-turbo-instruct' 'Please note that the text to be corrected is in French. Fix spelling mistakes, do not add/remove words, make consistent word spacing, add missing spaces, fix font case issues within words, fix numbering issues, make consistent line breaks for the following text:'
+Usage:
+    python src/main.py {'cjeu-35-turbo-instruct'|'cjeu-4'} 'system-prompt-string' 'user-prompt-string' [options]
+
+Options:
+    --indices   Index of files to be run. 
+    --sample    Percentage of input data to process. (1-100)%
+
+Examples:
+    python src/main.py 'cjeu-35-turbo-instruct' 'Consider the OCR output text and make corrections. The language is French.' 'Please note that the text to be corrected is in French. Fix spelling mistakes, do not add/remove words, make consistent word spacing, add missing spaces, fix font case issues within words, fix numbering issues, make consistent line breaks for the following text: Your text here' --indices 2 3 4 10 15 21
+    python src/main.py 'cjeu-4' 'Consider the OCR output text and make corrections. The language is French.' 'Please note that the text to be corrected is in French. Fix spelling mistakes, do not add/remove words, make consistent word spacing, add missing spaces, fix font case issues within words, fix numbering issues, make consistent line breaks for the following text: Your text here' --sample 20
+ 
  ```
 
 ## Results
 After running the script on input data, 
-- **textual results** can be obtained from `/data/fr/GPT_<timestamp>/result.txt`
+- **textual results** can be obtained from `/data/fr/GPT_<timestamp>/result.txt` and `/data/fr/GPT_<timestamp>/error_stat.txt`
 - **graphical results** can be obtained from `/data/GPT_<timestamp>.png`
 
-Note that every run will generate separate result files/folders that can be uniquely identified by a `<timestamp>`
+Note that every run will generate separate result files/folders that can be uniquely identified by a `<timestamp>`. 
 
+The rows in `result.txt` takes the following format:
+```python
+<fileid> <filename> <google-vision-edit-distance> <adobe-gpt-edit-distance>
+```
 ## Warning
 The use of OpenAI's API is not free and is subject to the pricing policies set by OpenAI. Usage costs are incurred based on the number of tokens processed. Detailed pricing information can be found on the OpenAI pricing page. If you choose to run or integrate this code, any and all charges incurred for API usage will be billed to the client of this specific project. We urge you to be mindful of potential bugs or unnecessary loops in the code that might lead to excessive and unintended API calls. Such issues can result in unexpected charges to the client. It's recommended to thoroughly test the code in a controlled environment before extensive usage.
-
 ## Issue Reporting
 If you encounter any bugs or issues while using the scripts, please report them in the Issues section. Include a brief description of the problem and steps to reproduce it. 
--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@
 
 """
 Script Name: main.py
-Description: This script is designed to run a specific GPT prompt for a specific engine to generate the .
+Description: Script to run a specific GPT prompt for a specific engine to generate the .
 Author: Sherin Sugathan
 Created Date: 26-12-2023
 Last Modified: 29-12-2023
 
-Usage: python script_name.py [arguments]
+Usage: python main.py [arguments]
 
 License: GPL
 """
@@ -28,6 +28,7 @@ from pathlib import Path
 from datetime import datetime
 import matplotlib.pyplot as plt
 import zipfile
+import numpy as np
 from scipy.interpolate import make_interp_spline
 
 def preprocess_text(text):
@@ -60,13 +61,34 @@ def normalized_levenshtein_distance(s1, s2):
     return normalized_distance
 
 
-def proc_text4(text_in):
+def proc_text4_notworking(text_in):
     # Call GPT-4 chat
     theprompt = prompt_prefix + '\n""""' + text_in + '"""'
+    tokenCount = len(prompt_prefix.split()) + len(text_in.split())
     openai.api_version = "2023-07-01-preview"
     response = openai.ChatCompletion.create(
         engine="cjeu-4",
-        messages=[{"role": "user", "content": theprompt}],
+        prompt=theprompt,
+        temperature=0.0,
+        max_tokens=tokenCount + 100,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None)
+
+    gpt_out = response.choices[0].message.text
+    #return gpt_out, response, theprompt
+    return gpt_out
+
+
+def proc_text4(text_in):
+    theprompt = prompt_prefix + '\n""""' + text_in + '"""'
+    #system_prompt = "Consider the OCR output text and make corrections. The language is French."
+    openai.api_version = "2023-07-01-preview"
+    response = openai.ChatCompletion.create(
+        engine="cjeu-4",
+        messages=[{"role": "system", "content": system_prompt},
+                  {"role": "user", "content": theprompt}],
         temperature=0.0,
         max_tokens=4000,
         top_p=0.95,
@@ -75,17 +97,20 @@ def proc_text4(text_in):
         stop=None)
 
     gpt_out = response.choices[0].message.content
-    return gpt_out, response, theprompt
+    return gpt_out
 
 def proc_text_instruct(text_in):
     # Call GPT-3.5 instruct
-    return "hello"
-    theprompt = prompt_prefix + '\n""""' + text_in + '"""'
+    tokenCount = len(prompt_prefix.split()) + len(text_in.split())
+    #system_prompt = "Consider the OCR output text and make corrections. The language is French."
+    theprompt = system_prompt + "\n" + prompt_prefix + '\n""""' + text_in + '"""'
+    tokenCount = len(prompt_prefix.split()) + len(text_in.split())
+    openai.api_version = "2023-05-15"
     response = openai.Completion.create(
         engine="cjeu-35-turbo-instruct",
         prompt=theprompt,
-        temperature=0.1,
-        max_tokens=4000,
+        temperature=0,
+        max_tokens=9999,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
@@ -100,12 +125,18 @@ def proc_text_instruct(text_in):
 parser = argparse.ArgumentParser(description='Run a prompt on cjeu french text.')
 # Adding arguments
 parser.add_argument('engine', type=str, choices=['cjeu-35-turbo-instruct', 'cjeu-4'], help='Name of the GPT Engine')
-parser.add_argument('prompt', type=str, help='The prompt string')
+parser.add_argument('sysprompt', type=str, help='The system prompt string')
+parser.add_argument('userprompt', type=str, help='The user prompt string')
+parser.add_argument('--sample', type=int, default=100, help='Input Sampling percentage value')
+parser.add_argument('--indices', nargs='*', type=int,  help='A list of specific file indices.')
+
 # Parsing arguments
 args = parser.parse_args()
 #prompt_prefix = "Please note that the text to be corrected is in French. Fix spelling mistakes, do not add/remove words, make consistent word spacing, add missing spaces, fix font case issues within words, fix numbering issues, make consistent line breaks for the following text:"
-prompt_prefix = args.prompt
+system_prompt = args.sysprompt
+prompt_prefix = args.userprompt
 engineSelection = args.engine
+percentage_to_process = args.sample
 
 current_dir = Path(__file__).resolve().parent
 parent_dir = current_dir.parent
@@ -126,15 +157,6 @@ if os.path.exists(zip_file_path) == False:
         # Extract the zip file
         extract_zip(zip_path)
 
-# Check if the folder exists
-if not os.path.exists(gpt_result_folder):
-    # Create the folder
-    os.makedirs(gpt_result_folder)
-    print(f"Folder created: {gpt_result_folder}")
-else:
-    print(f"Folder already exists: {gpt_result_folder}")
-
-
 # Set up API
 openai.api_type = "azure"
 openai.api_base = "https://gpt-cjeuscans-test-swe.openai.azure.com/"
@@ -150,19 +172,34 @@ new_distance_list = []
 file_list = [f for f in os.listdir(aocr_file) if os.path.isfile(os.path.join(aocr_file, f))]
 i = 0
 
+if args.indices:
+        file_list = [file_list[i] for i in args.indices]
+
+number_of_files_to_process = int(len(file_list) * (percentage_to_process / 100))
+if(number_of_files_to_process == 0):
+    print("Error: Insufficient number of input samples. Please check you arguments.")
+    quit()
+
+# Check if the folder exists
+if not os.path.exists(gpt_result_folder):
+    # Create the folder
+    os.makedirs(gpt_result_folder)
+else:
+    print(f"Folder already exists: {gpt_result_folder}")
+
 # Print the list of filenames
-for fileName in file_list:
+for fileName in file_list[:number_of_files_to_process]:
     i = i + 1
     while True:
         try:
             # Read the content of the text file
             file_path = aocr_file + fileName
-
-            if (i > len(file_list)):
+            if (i > len(file_list[:number_of_files_to_process])):
                 break
             image_name = fileName[:-3] + "jpg"
             result_file = gpt_result_folder + fileName
             distance_result_file = gpt_result_folder + "result.txt"
+            error_stat_file = gpt_result_folder + "error_stat.txt"
             extractedRow = df[df['image_file']==image_name]
             column_headers = extractedRow.columns.values.tolist()
 
@@ -213,8 +250,27 @@ plt.title('Normalized Levenshtein Distance comparison between Google Vision and 
 plt.legend()
 even_X_labels = [x for x in X if x % 2 == 0]
 plt.xticks(even_X_labels)
+plt.ylim(0, 1)
+plt.yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
 #plt.xticks(X)
 #plt.grid(True)
 plt.savefig(gpt_image_result_fileName, format='png', dpi=300, bbox_inches='tight', transparent=False, orientation ='landscape')
 plt.show()
+
+# Calculate basic statistics
+sum_error = np.sum(Y2)
+mean_error = np.mean(Y2)
+median_error = np.median(Y2)
+std_dev_error = np.std(Y2)
+variance_error = np.var(Y2)
+
+with open(error_stat_file, 'w') as file:
+    file.write(f"Sum of Errors: {sum_error}\n")
+    file.write(f"Mean Error: {mean_error}\n")
+    file.write(f"Median Error: {median_error}\n")
+    file.write(f"Standard Deviation: {std_dev_error}\n")
+    file.write(f"Variance: {variance_error}\n")
+
+print(f"Textual Result Location: {gpt_result_folder}")
+print(f"Graphical Result Location: {gpt_image_result_fileName}")
 print("###### COMPLETED ########")
